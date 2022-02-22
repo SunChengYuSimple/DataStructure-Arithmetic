c21253a3db1725256ac8a0ac01da618ec4aa6acb
Oliver Grande <N.Sunchengyu@gmail.com>
2022-02-22 17:54:10 +0800

    添加插入排序

--- src/main/java/selectionSort/InsertSort.java
+++ src/main/java/selectionSort/InsertSort.java
@@ -8,11 +8,11 @@ import java.util.Arrays;
  * @author 21051487 sunchengyu
  * @version 1.0
  * @date 2022/1/29
- * @description 选择排序算法
+ * @description 插入排序算法
  */
-public class SelectionSort {
+public class InsertSort {
 
-    private SelectionSort() {
+    private InsertSort() {
     }
 
     public static void main(String[] args) {
@@ -24,33 +24,27 @@ public class SelectionSort {
 
     public static <T extends Comparable<T>> void converseSort(T arr[]) {
         int arrayLength = arr.length;
-        for (int i = arrayLength - 1, minIndex; i >= 0; i--) {
-            minIndex = i;
-            for (int j = i; j >= 0; j--) {
-                if (arr[j].compareTo(arr[minIndex]) > 0) {
-                    minIndex = j;
+        for (int i = arrayLength; i >= 0; i--) {
+            for (int j = i; j < arrayLength - 1; j++) {
+                if (arr[j].compareTo(arr[j + 1]) > 0) {
+                    T temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
-
-            T temp = arr[i];
-            arr[i] = arr[minIndex];
-            arr[minIndex] = temp;
         }
     }
 
     public static <T extends Comparable<T>> void sort(T arr[]) {
         int arrayLength = arr.length;
-        for (int i = 0, minIndex; i < arrayLength; i++) {
-            minIndex = i;
-            for (int j = i; j < arrayLength; j++) {
-                if (arr[j].compareTo(arr[minIndex]) < 0) {
-                    minIndex = j;
+        for (int i = 0; i < arrayLength; i++) {
+            for (int j = i; j > 0; j--) {
+                if (arr[j].compareTo(arr[j - 1]) < 0) {
+                    T temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
                 }
             }
-
-            T temp = arr[i];
-            arr[i] = arr[minIndex];
-            arr[minIndex] = temp;
         }
     }
 }
